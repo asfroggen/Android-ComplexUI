e8598b2c96d55fbd6625975143f3d753d09eca6d
Esau <yosoypawn@hotmail.com>
2022-04-13 11:16:15 -0500

    canvas

--- app/src/main/java/com/esaudev/complexui/MainActivity.kt
+++ app/src/main/java/com/esaudev/complexui/MainActivity.kt
@@ -2,10 +2,17 @@ package com.esaudev.complexui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import com.esaudev.complexui.ui.component.CanvasView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myCanvasView = CanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
 }
